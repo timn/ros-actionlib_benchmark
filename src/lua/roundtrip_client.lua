b17d39582a77fa4a06b7cf431d8367bcbd93dfd6
Tim Niemueller <niemueller@kbsg.rwth-aachen.de>
2010-08-16 17:04:39 -0400

    Lua client: make more similar to other clients

--- src/lua/roundtrip_client.lua
+++ src/lua/roundtrip_client.lua
@@ -16,7 +16,6 @@ require("socket")
 roslua.init_node{master_uri=os.getenv("ROS_MASTER_URI"), node_name="/roundtrip_client"}
 
 local num_runs = tonumber(arg[1]) or 1
-local run = 1
 
 print()
 printf("Action client roundtrip benchmark (%d runs)", num_runs)
@@ -26,77 +25,70 @@ local acl = actionlib.action_client("/roundtrip", "actionlib_benchmark/Roundtrip
 acl:wait_for_server()
 
 local results = {}
+for i = 1, num_runs do
+   local goal = acl.actspec.goal_spec:instantiate()
+   goal.values.start = roslua.Time.now()
+   acl:send_goal(goal)
+   acl:wait_for_result()
 
-local goal = acl.actspec.goal_spec:instantiate()
-goal.values.start = roslua.Time.now()
+   local result = acl.result
 
-function calc_roundtrip(gh)
-   if gh.state == gh.SUCCEEDED then
-      local start = goal.values.start
-      local rcvd = roslua.Time:new(gh.result.values.result.values.received[1],
-				   gh.result.values.result.values.received[2])
-      local now = roslua.Time.now()
-      if num_runs == 1 then
-	 -- we only want the output and exit if there is only one run
-	 local difftime_send  = rcvd - start
-	 local difftime_recv  = now - rcvd
-	 local difftime_total = now - start
-	 printf("Sent:            %s (%d.%d)", tostring(start), start.sec, start.nsec)
-	 printf("Received:        %s (%d.%d)", tostring(rcvd), rcvd.sec, rcvd.nsec)
-	 printf("Now:             %s (%d.%d)", tostring(now), now.sec, now.nsec)
-	 printf("Sending time:    %f", difftime_send:to_sec())
-	 printf("Receiving time:  %f", difftime_recv:to_sec())
-	 printf("Round trip time: %f", difftime_total:to_sec())
-	 roslua.exit()
-      else -- multiple runs
-	 table.insert(results, {start=start, rcvd=rcvd, now=now})
-	 run = run + 1
-	 if run >= num_runs then
-	    local average_send    = 0
-	    local average_recv    = 0
-	    local average_total   = 0
-	    local deviation_send  = 0
-	    local deviation_recv  = 0
-	    local deviation_total = 0
-	    local n = #results
-
-	    for _, t in ipairs(results) do
-	       average_send  = average_send  + (t.rcvd - t.start):to_sec()
-	       average_recv  = average_recv  + (t.now  - t.rcvd):to_sec()
-	       average_total = average_total + (t.now  - t.start):to_sec()
-	    end
-	    average_send    = average_send / n
-	    average_recv    = average_recv / n
-	    average_total   = average_total / n
-
-	    for _, t in ipairs(results) do
-	       deviation_send = deviation_send +
-		  math.abs((t.rcvd - t.start):to_sec() - average_send)
-	       deviation_recv = deviation_recv +
-		  math.abs((t.now - t.rcvd):to_sec() - average_recv)
-	       deviation_total = deviation_total +
-		  math.abs((t.now - t.start):to_sec() - average_total)
-	    end
-	    deviation_send  = deviation_send / n
-	    deviation_recv  = deviation_recv / n
-	    deviation_total = deviation_total / n
-
-	    printf("SEND   Average: %f    Deviation: %f", average_send, deviation_send)
-	    printf("RECV   Average: %f    Deviation: %f", average_recv, deviation_recv)
-	    printf("TOTAL  Average: %f    Deviation: %f", average_total, deviation_total)
-
-	    roslua.exit()
-	 else
-	    -- post another goal
-	    --socket.sleep(0.05) -- 20 Hz
-	    goal = acl.actspec.goal_spec:instantiate()
-	    goal.values.start = roslua.Time.now()
-	    acl:send_goal(goal, calc_roundtrip)
-	 end
-      end
+   local start = goal.values.start
+   local rcvd = roslua.Time:new(result.values.result.values.received[1],
+				result.values.result.values.received[2])
+   local now = roslua.Time.now()
+   if num_runs == 1 then
+      -- we only want the output and exit if there is only one run
+      local difftime_send  = rcvd - start
+      local difftime_recv  = now - rcvd
+      local difftime_total = now - start
+      printf("Sent:            %s (%d.%d)", tostring(start), start.sec, start.nsec)
+      printf("Received:        %s (%d.%d)", tostring(rcvd), rcvd.sec, rcvd.nsec)
+      printf("Now:             %s (%d.%d)", tostring(now), now.sec, now.nsec)
+      printf("Sending time:    %f", difftime_send:to_sec())
+      printf("Receiving time:  %f", difftime_recv:to_sec())
+      printf("Round trip time: %f", difftime_total:to_sec())
+      roslua.exit()
+   else -- multiple runs
+      table.insert(results, {start=start, rcvd=rcvd, now=now})
    end
+
+   roslua.sleep(0.05)
 end
 
-local goal_id = acl:send_goal(goal, calc_roundtrip)
+if num_runs > 1 then
+   local average_send    = 0
+   local average_recv    = 0
+   local average_total   = 0
+   local deviation_send  = 0
+   local deviation_recv  = 0
+   local deviation_total = 0
+   local n = #results
 
-roslua.run()
+   for _, t in ipairs(results) do
+      average_send  = average_send  + (t.rcvd - t.start):to_sec()
+      average_recv  = average_recv  + (t.now  - t.rcvd):to_sec()
+      average_total = average_total + (t.now  - t.start):to_sec()
+   end
+   average_send    = average_send / n
+   average_recv    = average_recv / n
+   average_total   = average_total / n
+	 
+   for _, t in ipairs(results) do
+      deviation_send = deviation_send +
+	 math.abs((t.rcvd - t.start):to_sec() - average_send)
+      deviation_recv = deviation_recv +
+	 math.abs((t.now - t.rcvd):to_sec() - average_recv)
+      deviation_total = deviation_total +
+	 math.abs((t.now - t.start):to_sec() - average_total)
+   end
+   deviation_send  = deviation_send / n
+   deviation_recv  = deviation_recv / n
+   deviation_total = deviation_total / n
+
+   printf("SEND   Average: %f    Deviation: %f", average_send, deviation_send)
+   printf("RECV   Average: %f    Deviation: %f", average_recv, deviation_recv)
+   printf("TOTAL  Average: %f    Deviation: %f", average_total, deviation_total)
+end
+
+roslua.finalize()
